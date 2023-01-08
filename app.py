5e61eee50abbd4b283f32d1a30e89c468199282c
rrdentin <denti.dona@gmail.com>
2023-01-08 17:24:08 +0700

    flask setup for deployment

--- app.py
+++ app.py
@@ -1,8 +1,19 @@
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
 from flask import Flask, render_template, request, jsonify
 from pymongo import MongoClient
 
-client = MongoClient('mongodb+srv://test:sparta@cluster0.d5baau6.mongodb.net/?retryWrites=true&w=majority')
-db = client.dbsparta
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+MONGODB_URI = os.environ.get("MONGODB_URI")
+DB_NAME =  os.environ.get("DB_NAME")
+
+client = MongoClient(MONGODB_URI)
+
+db = client[DB_NAME]
 
 app = Flask(__name__)
 
@@ -12,6 +23,7 @@ def home():
 
 @app.route("/homework", methods=["POST"])
 def homework_post():
+    # sample_receive = request.form['sample_give']
     name_receive = request.form['name_give']
     comment_receive = request.form['comment_give']
     doc = {
@@ -19,7 +31,7 @@ def homework_post():
         'comment': comment_receive,
     }
     db.fanmessages.insert_one(doc)
-    return jsonify({'msg':'POST request!'})
+    return jsonify({'msg':'Comment Posted!'})
 
 @app.route("/homework", methods=["GET"])
 def homework_get():
